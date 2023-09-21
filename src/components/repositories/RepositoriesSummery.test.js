109f348f0d05585ee6d59a703453ecfabbe7c4fa
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-09-21 18:10:40 +0100

    Improve RepositoriesSummary Testing

--- src/components/repositories/RepositoriesSummery.test.js
+++ src/components/repositories/RepositoriesSummery.test.js
@@ -1,10 +1,12 @@
 import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("Should Display Repository Language", () => {
-    const repository = { stargazers_count: 1, open_issues: 1, forks: 5, language: "JavaScript" }
+test("Should Display Repository Details", () => {
+    const repository = { stargazers_count: 1, open_issues: 2, forks: 5, language: "JavaScript" }
     render(<RepositoriesSummary repository={repository} />);
 
-    const language = screen.getByText("JavaScript");
-    expect(language).toBeInTheDocument();
+    Object.keys(repository).forEach(key => {
+        const property = screen.getByText(new RegExp(repository[key]));
+        expect(property).toBeInTheDocument();
+    });
 });
